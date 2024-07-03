50fd85bee2a3f88c678e5c31c0bcfdd4ad6f0889
Dora314 <thienduy1001@gmail.com>
2024-07-03 18:45:45 +0700

    fix transaction in dashboard

--- src/controller/admin/dashboardController.js
+++ src/controller/admin/dashboardController.js
@@ -4,14 +4,14 @@ const getDashboardData = async (req, res) => {
   try {
     // Use database.pool to access the pool
     const [totalRevenueResult, totalCarsInStockResult] = await Promise.all([
-      database.pool.query('SELECT SUM(Transaction_Price) as total_revenue FROM dataACCOUTING'),
+      database.pool.query('SELECT SUM(Transaction_Price) as total_revenue FROM dataACCOUNTING'),
       database.pool.query('SELECT SUM(Car_Number_Availability) as total_cars_in_stock FROM dataCAR')
     ]);
 
     const totalRevenue = totalRevenueResult.rows[0].total_revenue;
     const totalCarsInStock = totalCarsInStockResult.rows[0].total_cars_in_stock;
 
-    const transactionDataResult = await database.pool.query('SELECT * FROM TransactionDashboard');
+    const transactionDataResult = await database.pool.query('SELECT * FROM dataTransaction ORDER BY Transaction_Date DESC LIMIT 5');
     const transactionData = transactionDataResult.rows;
 
     res.json({ totalRevenue, totalCarsInStock, transactionData });
